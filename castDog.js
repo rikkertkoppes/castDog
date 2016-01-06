f6f7dab549f302bc496531b5c322728f7746381c
rikkertkoppes <rikkert@rikkertkoppes.com>
2016-01-06 20:41:06 +0100

    cleanup

--- castDog.js
+++ castDog.js
@@ -62,19 +62,6 @@ Dog.prototype.getHost = function(name) {
 Dog.prototype.createBrowser = function() {
     var self = this;
     this.browser = mdns.createBrowser(mdns.tcp('googlecast'));
-
-    // this.browser.on('serviceUp', function(service) {
-    //     console.log('found device %s at %s:%d', service.name, service.addresses[0], service.port);
-    //     self.hosts[service.addresses[0]] = service;
-    //     console.log(self.hosts);
-    //     var deviceConfig = self.config[service.name];
-    //     // react if there is a configuration defined
-    //     if (deviceConfig) {
-    //         console.log('start service on ', service.name);
-    //         self.ondeviceup(service.addresses[0], deviceConfig);
-    //     }
-    // });
-
     this.browser.on('ready',() => this.browser.discover());
 
 
